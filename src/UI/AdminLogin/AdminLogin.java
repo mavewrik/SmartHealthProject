5becd5f3c555454088016efebf3101da96ee086c
Alvin-astro <alvin.dey@gmail.com>
2018-11-01 23:15:44 +0530

    Fourth Commit

--- src/UI/AdminLogin/AdminLogin.java
+++ src/UI/AdminLogin/AdminLogin.java
@@ -1,12 +1,27 @@
 package UI.AdminLogin;
 
-public class AdminLogin extends javax.swing.JFrame {
+import java.awt.event.*;
+import UI.Homepage.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class AdminLogin extends javax.swing.JFrame implements ActionListener{
 
     /**
      * Creates new form AdminLogin
      */
     public AdminLogin() {
+
         initComponents();
+        jButton1.addActionListener(this);
+        jButton2.addActionListener(this);
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent we)
+            {
+                System.exit(0);
+            }
+        });
+
     }
 
     /**
@@ -103,41 +118,27 @@ public class AdminLogin extends javax.swing.JFrame {
     private static void customFunction(String password) {
         
     }
-    
-    /**
-     * @param args the command line arguments
-     */
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new AdminLogin().setVisible(true);
-//            }
-//        });
-//    }
+    public void actionPerformed(ActionEvent ae)
+    { 	String s=ae.getActionCommand();
+        if(s.equals("BACK"))
+        {
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    new Homepage().setVisible(true);
+                }
+            });
+            this.setVisible(false);
+        }
+        else if(s.equals("LOGIN"))
+        {
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    //new PatientRegistration().setVisible(true);
+                }
+            });
+            this.setVisible(false);
+        }
+    }
 
     // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
--- src/UI/DoctorLogin/DoctorLogin.java
+++ src/UI/DoctorLogin/DoctorLogin.java
@@ -1,75 +1,79 @@
 /*
- * Created by JFormDesigner on Thu Nov 01 22:19:31 IST 2018
+ * Created by JFormDesigner on Thu Nov 01 22:40:59 IST 2018
  */
 
-package UI.Homepage;
+package UI.DoctorLogin;
+
+import UI.Homepage.Homepage;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.*;
-import java.awt.event.*;
-
-import UI.AdminLogin.*;
-import UI.DoctorLogin.DoctorLogin;
-import UI.PatientRegistration.*;
-import UI.PatientLogin.*;
 
 /**
  * @author Alvin Dey
  */
-public class Homepage extends JFrame implements ActionListener{
-    public Homepage() {
+public class DoctorLogin extends JFrame implements ActionListener {
+    public DoctorLogin() {
+
         initComponents();
         button1.addActionListener(this);
         button2.addActionListener(this);
-        button3.addActionListener(this);
-        button4.addActionListener(this);
         addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent we)
             {
                 System.exit(0);
             }
         });
-
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Alvin Dey
         label1 = new JLabel();
+        label2 = new JLabel();
+        label3 = new JLabel();
+        textField1 = new JTextField();
+        passwordField1 = new JPasswordField();
         button1 = new JButton();
         button2 = new JButton();
-        button3 = new JButton();
-        button4 = new JButton();
 
         //======== this ========
         Container contentPane = getContentPane();
         contentPane.setLayout(null);
 
         //---- label1 ----
-        label1.setText("SMART HEALTHCARE SYSTEM HOMEPAGE");
+        label1.setText("DOCTOR LOGIN");
         label1.setHorizontalAlignment(SwingConstants.CENTER);
         contentPane.add(label1);
-        label1.setBounds(60, 10, 340, label1.getPreferredSize().height);
+        label1.setBounds(110, 10, 180, label1.getPreferredSize().height);
+
+        //---- label2 ----
+        label2.setText("USERNAME");
+        contentPane.add(label2);
+        label2.setBounds(50, 60, 100, label2.getPreferredSize().height);
+
+        //---- label3 ----
+        label3.setText("PASSWORD");
+        contentPane.add(label3);
+        label3.setBounds(50, 110, 90, label3.getPreferredSize().height);
+        contentPane.add(textField1);
+        textField1.setBounds(170, 55, 170, textField1.getPreferredSize().height);
+        contentPane.add(passwordField1);
+        passwordField1.setBounds(170, 105, 170, passwordField1.getPreferredSize().height);
 
         //---- button1 ----
-        button1.setText("Patient Registration");
+        button1.setText("BACK");
         contentPane.add(button1);
-        button1.setBounds(30, 120, 180, button1.getPreferredSize().height);
+        button1.setBounds(80, 185, button1.getPreferredSize().width, 25);
 
         //---- button2 ----
-        button2.setText("Patient Login");
+        button2.setText("LOGIN");
         contentPane.add(button2);
-        button2.setBounds(30, 210, 180, button2.getPreferredSize().height);
-
-        //---- button3 ----
-        button3.setText("Admin Login");
-        contentPane.add(button3);
-        button3.setBounds(255, 120, 190, button3.getPreferredSize().height);
-
-        //---- button4 ----
-        button4.setText("Doctor Login");
-        contentPane.add(button4);
-        button4.setBounds(255, 210, 190, button4.getPreferredSize().height);
+        button2.setBounds(245, 185, button2.getPreferredSize().width, 25);
 
         { // compute preferred size
             Dimension preferredSize = new Dimension();
@@ -90,50 +94,31 @@ public class Homepage extends JFrame implements ActionListener{
     }
     public void actionPerformed(ActionEvent ae)
     { 	String s=ae.getActionCommand();
-        if(s.equals("Admin Login"))
+        if(s.equals("BACK"))
         {
             java.awt.EventQueue.invokeLater(new Runnable() {
                 public void run() {
-                    new AdminLogin().setVisible(true);
+                    new Homepage().setVisible(true);
                 }
             });
             this.setVisible(false);
         }
-        else if(s.equals("Patient Registration"))
+        else if(s.equals("LOGIN"))
         {
             java.awt.EventQueue.invokeLater(new Runnable() {
                 public void run() {
-                    new PatientRegistration().setVisible(true);
+                    //new PatientRegistration().setVisible(true);
                 }
             });
             this.setVisible(false);
         }
-        else if(s.equals("Patient Login"))
-        {
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    new PatientLogin().setVisible(true);
-                }
-            });
-            this.setVisible(false);
-        }
-        else if(s.equals("Doctor Login"))
-        {
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    new DoctorLogin().setVisible(true);
-                }
-            });
-            this.setVisible(false);
-        }
-
     }
-    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Alvin Dey
     private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
     private JButton button1;
     private JButton button2;
-    private JButton button3;
-    private JButton button4;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
